708655ecf61f406e4e79f78908342cd249a33bea
Edgar Huamantla <edgar@huamantla.biz>
2023-02-05 23:02:14 -0500

    added top level statement in csharp to call inheritance.main()

--- inheritance/Program.cs
+++ inheritance/Program.cs
@@ -5,13 +5,15 @@ using System;
 //  static and dynamic type information, how they're used.
 //   different kind of dispatch, when exactly does dynamic dispatch occur
 //   virtual-override versus new
-//   variable scope stays static, dynamic dispatch only on virtual methods.
+;//   variable scope stays static, dynamic dispatch only on virtual methods.
 //   difference between dynamic dispatch and overloading
 //   type casting; compiler versus runtime errors
 //  how does C# differ from Java and from C++?
 ////////
 
-AA m = new BB(1,2);
+inheritance.Main();
+
+/*AA m = new BB(1,2);
 // if (...) m = new BB(1,2); else m = new AA(1);
 m.f();  //  AA.f   // static dispatch
 m.g();  //  BB.g,x==2  // dynamic dispatch
@@ -19,9 +21,9 @@ m.g();  //  BB.g,x==2  // dynamic dispatch
 //m = new AA(1);
 ((BB)m).h();  
 
-Inherit myObj = new Inherit();
 
-myObj.q(m);
+
+//myObj.q(m);
 
 //Main
     
@@ -46,13 +48,6 @@ namespace myInheritance
 	public override void g() {Console.WriteLine("BB.g, my x=="+x);} //override
 	public void h() {Console.WriteLine("BB.h");}  // completely new
     }//BB
-
-    class Inherit
-    {
-	public Inherit() {}
-	public static void q(BB x) {Console.WriteLine("q(BB)");} //new
-	public static void q(AA x) {Console.WriteLine("q(AA)");}	
-    }
-}
+    }*/
 
 
